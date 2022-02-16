984cf3358b94b947bf7a2e075e9624bdaae31623
Vadzim.Yautseyeu <vadimevteev0@gmail.com>
2022-02-16 14:01:56 +0300

    CASE-2: Add ProjectData.cs

---
+++ TestRailAutomationTest/Model/Project/ProjectData.cs
@@ -0,0 +1,13 @@
+using Bogus;
+using TestRailAutomationTest.Model.Project.Enum;
+using TestRailAutomationTest.Utils;
+
+namespace TestRailAutomationTest.Model.Project;
+
+public static class ProjectData
+{
+    public static ProjectType GetRandomProjectType()
+    {
+        return RandomData.Faker.PickRandom<ProjectType>();
+    }
+}
